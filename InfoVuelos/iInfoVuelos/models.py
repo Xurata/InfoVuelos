ba05e3ba5cb85180cc5820a7e728e6daba547655
jbp10 <jbelluscio@gmail.com>
2014-03-22 10:43:15 +0100

    Primer contacto de la aplicacion

---
+++ InfoVuelos/iInfoVuelos/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Airport(models.Model):
+	
+	code = models.ForeignKey(max_length=3)
+	name = models.TextField(max_length=100)
+	date = models.DateTimeField()
+	#amount = models.IntegerField()
+	#concept = models.TextField(max_length=100)
+
+class Company (models.Model):
+
+	code = models.ForeignKey(max_legth=2)	
+	airport = models.ForeignKey(Airport)
+	name = models.TextField(max_length=100)	
+	#date = models.DateTimeField()
+	#amount = models.IntegerField()
+	#concept = models.TextField(max_length=100)
+
+class Flight (models.Model):
+
+	code= models.ForeingKey(max_length=5)	
+	company = model.ForeingKey(Company)
+	origin = models.TextField(max_length=3)
+	destination = models.TextField(max_length=3)
+	date = models.DateField()
+	time = models.TimeField()
+	gate = models.TexField(max_length=3)
+	
+
