938162fb487979813e37174ae8d65c30642b9ad2
jbp10 <jbelluscio@gmail.com>
2014-03-22 13:39:11 +0100

    Guardando

---
+++ InfoVuelos/iInfoVuelos/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from iInfoVuelos.models import Airport
+from iInfoVuelos.models import Company
+from iInfoVuelos.models import Flight
+
+admin.site.register(Airport)
+admin.site.register(Company)
+admin.site.register(Flight)
